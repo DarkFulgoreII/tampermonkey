07e9a918f0cd7a59cb892513158730f33217d6a0
DarkFulgoreII <DarkFulgoreII@gmail.com>
2024-08-26 18:18:42 -0500

    Commit inicial de la herramienta para descarga de reportes de leap

---
+++ leap-reports_downloader/leap-reports-downloader.js
@@ -0,0 +1,143 @@
+// ==UserScript==
+// @name         Leap reports downloader
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  automated downloads for brightspace leap activities
+// @author       You
+// @match        https://leaplti-es.desire2learn.com/Teacher/*/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=coursera.org
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    // Wait for the page to fully load
+    window.addEventListener('load', createControls);
+
+})();
+
+function downloadReports()
+{
+    let currentUrl = window.location.href;
+    var reportType = getReportType(currentUrl);
+    var results = {};
+    var tableName = "";
+    if (reportType =="StudentOutcomesSummaryReport")
+    {
+        tableName = "OutcomeSummaryTable";
+    }
+    else if (reportType =="ContentSummaryReport")
+    {
+        tableName = "ContentSummaryTable";
+    }
+    else if (reportType =="ActivitySummaryReport")
+    {
+        tableName = "ActivitySummaryTable";
+    }
+    var tableRows = document.querySelectorAll("#"+tableName+" > tbody > tr > td.rating.row-header");
+    for (var ori = 0 ; ori< tableRows.length ; ori++)
+    {
+        //for each outcome row
+        var ore = tableRows[ori];
+        var orlink = ore.childNodes[1].childNodes[1].href;
+        var orname= ore.childNodes[1].childNodes[1].innerHTML;
+        var detling = generateDetailsDownloadLink(reportType, orlink);
+        results[orname]=detling;
+    }
+    console.log(results);
+    downloadAllReports(reportType, results);
+    //var csv=generateCSVreport(results);
+    //download("report.csv", csv);
+}
+function getReportType(currentURL)
+{
+    //https://leaplti-es.desire2learn.com/Teacher/StudentOutcomesSummaryReport/1765389/25869
+    //https://leaplti-es.desire2learn.com/Teacher/ContentSummaryReport/1765389/25869
+    //https://leaplti-es.desire2learn.com/Teacher/ActivitySummaryReport/1765389/25869
+    if (currentURL.indexOf("/StudentOutcomesSummaryReport/")>0)
+    {
+        return "StudentOutcomesSummaryReport";
+    }
+    if (currentURL.indexOf("/ContentSummaryReport/")>0)
+    {
+        return "ContentSummaryReport";
+    }
+    if (currentURL.indexOf("/ActivitySummaryReport/")>0)
+    {
+        return "ActivitySummaryReport";
+    }
+}
+function generateDetailsDownloadLink(reportType, orlink)
+{
+    //first: extract data
+    var p1 = orlink.slice(orlink.indexOf("/"+reportType+"/")+"/"+reportType+"/".length);
+    var lessonid = p1.slice(0,p1.indexOf("/"));
+    var p2 = p1.slice(p1.indexOf("/"));
+    var taxid = p2.slice(p2.indexOf("lo_taxonomyId=")+"lo_taxonomyId=".length);
+    var reportTypePath = "";
+    if (reportType =="StudentOutcomesSummaryReport")
+    {
+        //https://leaplti-es.desire2learn.com/Teacher/StudentOutcomesSummaryAsCSV/1765389?sort=LastName&sortdir=asc&lo_taxonomyId=1765392&isDetail=True
+        reportTypePath = "StudentOutcomesSummaryAsCSV";
+    }
+    else if (reportType =="ContentSummaryReport")
+    {
+        //https://leaplti-es.desire2learn.com/Teacher/StudentContentSummaryAsCSV/1765389?sort=Name&sortdir=asc&lo_taxonomyId=1765392&isDetailByContent=True
+        reportTypePath = "StudentContentSummaryAsCSV";
+    }
+    else if (reportType =="ActivitySummaryReport")
+    {
+        //https://leaplti-es.desire2learn.com/Teacher/ActivitySummaryReportAsCsv/1765389?lo_taxonomyId=1765392&sort=LastName&sortdir=asc&isDetail=True
+        reportTypePath = "ActivitySummaryReportAsCsv";
+    }
+
+    var newlink = "https://leaplti-es.desire2learn.com/Teacher/"+reportTypePath+"/";
+    newlink += lessonid+"?sort=LastName&sortdir=asc&lo_taxonomyId="+taxid+"&isDetail=True";
+
+    return newlink;
+}
+function downloadAllReports(reportType, results)
+{
+    for (let key in results)
+    {
+        //console.log(`${key}: ${results[key]}`);
+        downloadBlob(reportType+" "+key+".csv", results[key]);
+    }
+}
+
+function downloadBlob(filename, link) {
+    fetch(link)
+        .then(response => response.blob())
+        .then(blob => {
+            var url = window.URL.createObjectURL(blob);
+            var element = document.createElement('a');
+            element.setAttribute('href', url);
+            element.setAttribute('download', filename);
+
+            element.style.display = 'none';
+            document.body.appendChild(element);
+
+            element.click();
+
+            document.body.removeChild(element);
+            window.URL.revokeObjectURL(url); // Free up memory
+        })
+        .catch(error => console.error('Error downloading file:', error));
+}
+
+function createControls()
+{
+    var where = document.querySelector("div.tab-content > div:nth-child(2)");
+
+    var buttonfilter= document.createElement("button");
+    buttonfilter.innerHTML="Download detailed reports ";
+    buttonfilter.setAttribute("style","background-color: #1c61a7;border: none;color: white;padding: 10px 20px;text-align: center;text-decoration: none;display: inline-block;font-size: 12px;border-radius: 8px;");
+    buttonfilter.setAttribute("class","paginate_button");
+    buttonfilter.addEventListener( 'click', function()
+    {
+        downloadReports();
+    });
+
+    where.appendChild(buttonfilter);
+    console.log("Components created");
+}
--- readme.md
+++ readme.md
@@ -3,3 +3,4 @@ There are some tampermonkey scripts. Works on Chrome based browsers.
 
  - Coursera items listing : Allows to download a filtered list of items from content edit page on courses in Coursera. This script works only for spanish language.
  - Coursera labs listing : Allows to download a filtered list of labs from lab manager page on courses in Coursera. This script works only for spanish language.
+ - Leap reports downloader : Allows to download reports from Brightspace Leap's lessons.
